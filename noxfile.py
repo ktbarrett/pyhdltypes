0cef233e86c6c6a532863eaa967185ec78a80539
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-07-30 16:54:14 -0500

    Add support for typechecking in pytest

--- noxfile.py
+++ noxfile.py
@@ -4,5 +4,5 @@ import nox
 @nox.session
 def tests(session):
     session.install(".")
-    session.install("pytest", "pytest-coverage", "coverage")
-    session.run("pytest", "--cov=hdltypes", "--cov-branch", "tests/", *session.posargs)
+    session.install("pytest", "pytest-coverage", "coverage", "pytest-mypy-plugins")
+    session.run("pytest", "--cov=hdltypes", "--cov-branch", *session.posargs)
