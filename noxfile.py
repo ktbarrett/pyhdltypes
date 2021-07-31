2f522d341521489b572d215ae401b817d0ade5c8
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-07-31 15:40:29 -0500

    Add sphinx documentation

--- noxfile.py
+++ noxfile.py
@@ -7,3 +7,10 @@ def tests(session):
     session.install("pytest", "pytest-coverage", "coverage", "pytest-mypy-plugins")
     session.run("pytest", "--cov=hdltypes", "--cov-branch", *session.posargs)
     session.run("coverage", "xml")
+
+
+@nox.session
+def docs(session):
+    session.install(".")
+    session.install("-r", "docs/requirements.txt")
+    session.run("sphinx-build", "docs/", "docs_out/", *session.posargs)
