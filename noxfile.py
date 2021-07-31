5f7f3d864f1d51e7d21ae363013c7f7623bf29f0
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-07-30 22:32:08 -0500

    Fix coverage upload

--- noxfile.py
+++ noxfile.py
@@ -6,3 +6,4 @@ def tests(session):
     session.install(".")
     session.install("pytest", "pytest-coverage", "coverage", "pytest-mypy-plugins")
     session.run("pytest", "--cov=hdltypes", "--cov-branch", *session.posargs)
+    session.run("coverage", "xml")
