3486fe3766da450ab8cc60cef8efc59f48576b87
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-12-28 13:59:11 -0600

    Simplify versioning

--- src/hdltypes/version.py
+++ src/hdltypes/version.py
@@ -1,9 +1,1 @@
-major = "0"
-minor = "2"
-patch = "0"
-release = "dev0"
-
-__version__ = f"{major}.{minor}.{patch}"
-
-if release != "":  # pragma: no cover
-    __version__ += f".{release}"
+__version__ = "0.dev2"
