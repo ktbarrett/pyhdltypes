a1fd8c14593c36b59f7a7e753e62e267fb6ff347
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-12-28 14:38:51 -0600

    Add re-exports to __init__.py

---
+++ src/hdltypes/__init__.py
@@ -0,0 +1,5 @@
+from hdltypes.array import Array  # noqa: F401
+from hdltypes.logic import X01Z, Bit, StdLogic  # noqa: F401
+from hdltypes.logic_array import BitArray, StdLogicArray, X01ZArray  # noqa: F401
+from hdltypes.range import Range  # noqa: F401
+from hdltypes.version import __version__  # noqa: F401
